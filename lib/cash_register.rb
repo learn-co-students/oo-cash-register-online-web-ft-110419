83413f0f20188c3e89b4fbbbe32bee438d53ffb3
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-20 21:19:03 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require "pry"
+class CashRegister
+  attr_accessor :title, :price, :apply_discount
+  attr_writer :total
+  def initialize(title=nil,apply_discount=0)
+    @total = 0
 
+  end
+  def add_item(title,price,quantity=1)
+    @total = @total + @price*quantity
+    @total
+  end
+  def apply_discount
+      "There is no discount to apply."
+  end
+  def total
+    @total
+  end
+
+end
